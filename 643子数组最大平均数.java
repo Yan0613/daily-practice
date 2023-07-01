3ea3a7bd91f760a2df3eeb7fdbb8b9f028bff954
Yan0613 <1020303879@qq.com>
2023-07-01 21:52:55 +0800

    '7.1'

---
+++ 643子数组最大平均数.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return 1.0 * maxSum / k;
+    }
+}
