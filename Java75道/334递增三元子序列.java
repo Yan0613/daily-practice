7eb77b64164d1bdc2068fcaee1f19f2f84eac3e7
Yan0613 <1020303879@qq.com>
2023-08-14 17:08:24 +0800

    'update'

--- Java75道/334递增三元子序列.java
+++ Java75道/334递增三元子序列.java
@@ -18,8 +18,3 @@ class Solution {
         return false;
     }
 }
-
-作者：力扣官方题解
-链接：https://leetcode.cn/problems/increasing-triplet-subsequence/solutions/1204375/di-zeng-de-san-yuan-zi-xu-lie-by-leetcod-dp2r/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
