d7c8ec1ba4da2057bb82d2ada8bbb7281a37d119
Yan0613 <1020303879@qq.com>
2023-08-14 18:28:38 +0800

    'rea'

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -4,9 +4,12 @@ function ListGroup() {
     return (
         <>
             <h1>List</h1>
+            {items.length === 0 ? <p>No item</p> : null}
             <ul className="list-group">
                 {items.map((item) => (<li key={item}>{item}</li>))}
             </ul>
         </>
     )
 }
+
+export default ListGroup;
