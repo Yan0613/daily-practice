6757c98d820130193e402d5c6b27de29d4f49621
Yan0613 <1020303879@qq.com>
2023-12-11 16:05:30 +0800

    '12.11'

---
+++ leetcode-master/哈希表/ThreeSum.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+
+    public List<List<Integer>> threeSumso(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] + nums[left] + nums[right] > 0) {
+                right--;
+            } else if (nums[i] + nums[left] + nums[right] < 0) {
+                left++;
+            } else {
+                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                left++;
+                right--;
+            }
+        }
+        return result;
+    }
+
+}
