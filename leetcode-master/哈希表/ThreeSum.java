f99409eb444facb03af69cddfc45d7717e750238
Yan0613 <1020303879@qq.com>
2023-12-11 16:37:40 +0800

    '12.10'

--- leetcode-master/哈希表/ThreeSum.java
+++ leetcode-master/哈希表/ThreeSum.java
@@ -16,6 +16,12 @@ public class ThreeSum {
                 left++;
             } else {
                 result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                while (right > left && nums[right] == nums[right - 1]) {
+                    right--;
+                }
+                while (right > left && nums[left] == nums[left + 1]) {
+                    left++;
+                }
                 left++;
                 right--;
             }
