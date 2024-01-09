787d2a678cf1c5c1b6a71a9a8e57d73eb9fcdbee
Yan0613 <1020303879@qq.com>
2024-01-09 20:04:07 +0800

    '1.9'

---
+++ leetcode-master/贪心算法/无重叠区间.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)-> {
+            return Integer.compare(a[0],b[0]);
+        });
+        int ans = 0;
+        if(intervals.length == 1){
+            ans = 0;
+        }
+        for(int i = 1; i < intervals.length;i++){
+            if(intervals[i][0] >= intervals[i-1][1]){
+                //continue
+            }else{
+                //移除
+                intervals[i][1] = Math.min(intervals[i][1],intervals[i-1][1]);
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
