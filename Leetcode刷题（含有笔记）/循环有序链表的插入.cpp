ecf6059ca9e776bd6c8ed4ed992b0e2bf8a9a326
Yan0613 <1020303879@qq.com>
2024-01-17 19:34:24 +0800

    '1.17'

---
+++ Leetcode刷题（含有笔记）/循环有序链表的插入.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        if (head == null) {
+            Node res = new Node(insertVal);
+            res.next = res;
+            return res;
+        }
+
+        Node pre = head;
+        Node curr = head.next;
+
+        while (!(pre.val <= insertVal && insertVal <= curr.val) &&
+               !(pre.val > curr.val && (insertVal >= pre.val || insertVal <= curr.val))) {
+            pre = pre.next;
+            curr = curr.next;
+
+            // 如果遍历一圈仍未找到插入位置，说明所有元素相等，可以在任意位置插入
+            if (pre == head) {
+                break;
+            }
+        }
+
+        Node newNode = new Node(insertVal);
+        pre.next = newNode;
+        newNode.next = curr;
+
+        return head;
+    }
+}
