61058c7c458ba506a0638d3b88911384d867715a
Yan0613 <1020303879@qq.com>
2024-01-19 23:33:40 +0800

    '1.19'

--- leetcode-master/动态规划/最后一块石头的重量.cpp
+++ leetcode-master/动态规划/最后一块石头的重量.cpp
@@ -1,12 +1,19 @@
-class Solution {
+// 本题其实就是尽量让石头分成重量相同的两堆，相撞之后剩下的石头最小，这样就化解成01背包问题了。
+
+class Solution
+{
 public:
-    int lastStoneWeightII(vector<int>& stones) {
+    int lastStoneWeightII(vector<int> &stones)
+    {
         vector<int> dp(15001, 0);
         int sum = 0;
-        for (int i = 0; i < stones.size(); i++) sum += stones[i];
+        for (int i = 0; i < stones.size(); i++)
+            sum += stones[i];
         int target = sum / 2;
-        for (int i = 0; i < stones.size(); i++) { // 遍历物品
-            for (int j = target; j >= stones[i]; j--) { // 遍历背包
+        for (int i = 0; i < stones.size(); i++)
+        { // 遍历物品
+            for (int j = target; j >= stones[i]; j--)
+            { // 遍历背包
                 dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
             }
         }
