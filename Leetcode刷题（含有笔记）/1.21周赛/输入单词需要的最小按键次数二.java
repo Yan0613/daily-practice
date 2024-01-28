1d2aa311ae5e9bbfb99e312846530644920c1953
Yan0613 <1020303879@qq.com>
2024-01-28 17:19:44 +0800

    '1.28'

--- Leetcode刷题（含有笔记）/1.21周赛/输入单词需要的最小按键次数二.java
+++ Leetcode刷题（含有笔记）/1.21周赛/输入单词需要的最小按键次数二.java
@@ -1,38 +1,36 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 class Solution {
     public int minimumPushes(String word) {
-        // 统计每个字母出现的次数
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            int v = map.get(c);
-            if (v == 0) {
-                map.put(c, 1);
-            } else {
-                v++;
-                map.put(c, v);
-            }
+        // 可用的只有2~9共8个按键
+        int keyNum = 8;
+        // 1、统计各字符出现次数
+        HashMap<Character, Integer> charCntMap = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            charCntMap.put(c, charCntMap.getOrDefault(c, 0) + 1);
         }
-        List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(map.entrySet());
-        entryList.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
 
-        int res = 0;
-        for (int i = 0; i < ((CharSequence) entryList).length(); i++) {
-            int value = entryList.get(i).getValue();
-            if (i <= 8 - 1) {
-                res += value;
-            } else if (i > 7 && i <= 15) {
-                res += 2 * value;
-            } else if (i > 15 && i <= 24) {
-                res += 3 * value;
-            } else if (i > 24 && i <= 26) {
-                res += 4 * value;
+        // 2、按值从大到小排序
+        List<Integer> charCntList = new ArrayList<>(charCntMap.values());
+        charCntList.sort((i1, i2) -> {
+            return i2.compareTo(i1);
+        });
+
+        // 3、累加。使用贪心策略，点击次数越多的字母，分配在每个按键的位置就越靠前
+        int rs = 0;
+        // 分配在每个按键的位置
+        int index = 0;
+        for (int i = 1; i <= charCntList.size(); i++) {
+            if (i % keyNum == 1) {
+                // 上一个位置都已经分配完
+                index++;
             }
+            // 例：c被分配在按键的第index位，则每次c要按index次；word中共有charCntList.get(i)个c
+            rs += index * charCntList.get(i - 1);
         }
-        return res;
+
+        return rs;
     }
 }
--- Python75道/151.反转字符串.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        return " ".join(s.strip().split()[::-1]);
-
-#"".join,""是用于拼接的字符串，join参数可以是数组
-#split函数，参数可以是正则，在正则表达式中"\d+"表示一个或多个数字，是用于从一堆数字字母以及其它字符组成的字符串中获取非数字字符或字符串。
