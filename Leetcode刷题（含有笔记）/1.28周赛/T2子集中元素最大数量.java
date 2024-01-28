491431e7abcf5f8e3f54fa66e601a0cbe4c23742
Yan0613 <1020303879@qq.com>
2024-01-28 16:10:28 +0800

    '1.28'

--- Leetcode刷题（含有笔记）/1.28周赛/T2子集中元素最大数量.java
+++ Leetcode刷题（含有笔记）/1.28周赛/T2子集中元素最大数量.java
@@ -1,20 +1,26 @@
+import java.util.TreeMap;
+
 class Solution {
     public static int maximumLength(int[] nums) {
-        //Arrays.sort(nums);
+        // Arrays.sort(nums);
         TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-        for (int i : nums) treeMap.put(i, treeMap.getOrDefault(i, 0) + 1);
+        for (int i : nums)
+            treeMap.put(i, treeMap.getOrDefault(i, 0) + 1);
 
-        //1要特殊处理一下。
+        // 1要特殊处理一下。
         int res = 1;
         if (treeMap.containsKey(1) && treeMap.get(1) >= 3) {
             int value = treeMap.get(1);
-            if (value % 2 == 0) res = value - 1; //偶数个 那就是个数减一个，因为山峰不能配对，所以要减一。
-            if (value % 2 == 1) res = value; //奇数个 那就是本身。
+            if (value % 2 == 0)
+                res = value - 1; // 偶数个 那就是个数减一个，因为山峰不能配对，所以要减一。
+            if (value % 2 == 1)
+                res = value; // 奇数个 那就是本身。
         }
         treeMap.remove(1);
 
         for (int i : treeMap.keySet()) {
-            if (handle(i) == -1) continue;
+            if (handle(i) == -1)
+                continue;
 
             int cnt = 1;
             int now = i;
@@ -27,11 +33,11 @@ class Solution {
         return res;
     }
 
-    //开不了，那当前就是最开始的x，那就返回-1？
+    // 开不了，那当前就是最开始的x，那就返回-1？
     public static int handle(int x) {
         double sq = Math.sqrt(x);
-        if ((int)sq * (int)sq == x)
-            return (int)sq;
+        if ((int) sq * (int) sq == x)
+            return (int) sq;
 
         return -1;
     }
