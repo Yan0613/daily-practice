791479f52b1d0da7944f52578be55186710c5b6d
Yan0613 <1020303879@qq.com>
2024-05-22 14:10:21 +0800

    '5.22'

---
+++ Go刷题/LC796旋转字符串.go
@@ -0,0 +1,8 @@
+func rotateString(s string, goal string) bool {  
+    if len(s) != len(goal) {  
+        return false // 长度不同的字符串不可能通过旋转匹配  
+    }  
+    return strings.Contains(s+s, goal)  
+}  
+  
+// 注意：你需要导入 "strings" 包来使用 strings.Contains 函数
